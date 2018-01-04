18b595a27cbf492fa4ba1090dc7243c59373af76
Jessica Gross <Jessicasarahgross@gmail.com>
2018-01-04 15:08:46 -0500

    M2 DVD Lib

---
+++ com.sg_Milestone2AssessmentDvDLib_jar_1.0-SNAPSHOT/src/main/java/com/sg/dvdlib/dto/DVDLib.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.dvdlib.dto;
+
+/**
+ *
+ * @author jesse
+ */
+public class DVDLib {
+    private final String title;
+    private String genre;
+    private String year;
+    private String studio;
+    private String directorsName;
+    private String userRating; 
+    private String mpaaRating;
+
+    public DVDLib(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
+
+    public String getDirectorsName() {
+        return directorsName;
+    }
+
+    public void setDirectorsName(String directorsName) {
+        this.directorsName = directorsName;
+    }
+
+    public String getUserRating() {
+        return userRating;
+    }
+
+    public void setUserRating(String userRating) {
+        this.userRating = userRating;
+    }
+
+    public String getMpaaRating() {
+        return mpaaRating;
+    }
+
+    public void setMpaaRating(String mpaaRating) {
+        this.mpaaRating = mpaaRating;
+    }
+    
+}
